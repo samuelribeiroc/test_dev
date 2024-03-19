80ca1045b130d0cf93db5c566c4eac57f865200a
Samuel Costa <samuel.costa@ccc.ufcg.edu.br>
2024-03-19 08:49:40 -0300

    fix: fixing eslint settings

--- src/index.ts
+++ src/index.ts
@@ -5,39 +5,39 @@ const logger = debug('core');
 
 const delays = [...Array(50)].map(() => Math.floor(Math.random() * 900) + 100);
 const load = delays.map(
-    (delay) => (): Promise<number> =>
-        new Promise((resolve) => {
-            setTimeout(() => resolve(Math.floor(delay / 100)), delay);
-        }),
+	(delay) => (): Promise<number> =>
+		new Promise((resolve) => {
+			setTimeout(() => resolve(Math.floor(delay / 100)), delay);
+		}),
 );
 
 type Task = () => Promise<number>;
 
 async function processTask(queue: Task[], results: number[]): Promise<void> {
-    while (queue.length > 0) {
-        const task = queue.shift()!;
-        const result = await task();
-        results.push(result);
-    };
-};
+	while (queue.length > 0) {
+		const task = queue.shift()!;
+		const result = await task();
+		results.push(result);
+	}
+}
 
 const throttle = async (workers: number, tasks: Task[]) => {
-    const queue: Task[] = [...tasks];
-    const results: number[] = [];
-    const workersPromises = Array.from({ length: workers }).map(() => processTask(queue, results));
+	const queue: Task[] = [...tasks];
+	const results: number[] = [];
+	const workersPromises = Array.from({ length: workers }).map(() => processTask(queue, results));
 
-    await Promise.all(workersPromises);
-    return results;
+	await Promise.all(workersPromises);
+	return results;
 };
 
 const bootstrap = async () => {
-  logger('Starting...');
-  const start = Date.now();
-  const answers = await throttle(5, load);
-  logger('Done in %dms', Date.now() - start);
-  logger('Answers: %O', answers);
+	logger('Starting...');
+	const start = Date.now();
+	const answers = await throttle(5, load);
+	logger('Done in %dms', Date.now() - start);
+	logger('Answers: %O', answers);
 };
 
 bootstrap().catch((err) => {
-  logger('General fail: %O', err);
+	logger('General fail: %O', err);
 });
